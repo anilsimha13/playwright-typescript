1b935124153db6df860968cdf2ceb327ed49abe2
anilsimha13 <tanilsimha@gmail.com>
2026-03-20 12:50:29 +0530

    Day-06

--- e2e/tests/addEmp.spec.ts
+++ e2e/tests/addEmp.spec.ts
@@ -3,7 +3,7 @@ import { LoginPage } from "./pages/loginPage";
 import { HomePage } from "./pages/homePage";
 import { AddEmp } from "./pages/addEmpPage";
 
-test.only("test", async ({ page }) => {
+test("test", async ({ page }) => {
   const loginPage = new LoginPage(page);
   const homePage = new HomePage(page);
   const addEmp = new AddEmp(page);
